c5f5fac88c76ceab5e24808a759a2f9947451a79
phamkevinusa <134666009+phamkevinusa@users.noreply.github.com>
2023-08-17 16:34:29 -0500

    Add files via upload

--- EOY_Pham/Runner.java
+++ EOY_Pham/Runner.java
@@ -1,11 +1,7 @@
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import java.awt.CardLayout;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
 
 public class Runner {
     public static void main(String[] args) throws InterruptedException {
